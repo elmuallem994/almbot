265c66caaa369e23995753de74a07559ef7d8243
almllm994 <elmuallem994@gmail.com>
2025-02-10 01:30:52 +0300

    🚀 تحسين تحميل فيديوهات YouTube وحل مشكلة تسجيل الدخول

--- bot.py
+++ bot.py
@@ -80,15 +80,15 @@ async def handle_video_download(query, url, unique_id):
         os.remove(output_video)
 
     ydl_opts = {
-    "format": "bestvideo[height<=480]+bestaudio/best[height<=480]",  
+    "format": "worstvideo+worstaudio/worst",  # ✅ تحميل أقل جودة متاحة
     "merge_output_format": "mp4",
     "outtmpl": output_video,
     "socket_timeout": 3600,
     "retries": 30,
     "fragment_retries": 30,
     "hls_prefer_native": True,
-    "nocheckcertificate": True,  # ✅ تخطي التحقق من الشهادات
 }
+
 
 
     try:
